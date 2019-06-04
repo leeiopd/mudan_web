4462c2785288598ce7cee2a5eb7208e17999846b
leeiopd <leeiopd@hanmail.net>
2019-06-04 16:14:08 +0900

    Videos|리스트페이지 검색기능 추가(Team 검색)

--- videos/views.py
+++ videos/views.py
@@ -7,23 +7,35 @@ def list(request):
     onenotice  = get_object_or_404(Onenotice, pk=1)
     years = Year.objects.all()
     concerts = Concert.objects.all()
+    teams = Team.objects.all()
 
     if request.method == 'POST':
         search_year = request.POST.get('search_year')
         search_concert = request.POST.get('search_concert')
-        if search_year == 'all' and search_concert == 'all':
+        search_team = request.POST.get('search_team')
+        if search_year == 'all' and search_concert == 'all' and search_team == 'all':
             videos = Video.objects.all().order_by('-year')
-        if search_year != 'all' and search_concert == 'all':
+        if search_year != 'all' and search_concert != 'all' and search_team != 'all':
+            videos = Video.objects.filter(year_id = search_year, concert_id = search_concert, team_id = search_team).order_by('-year')
+
+        if search_year != 'all' and search_concert == 'all' and search_team == 'all':
             videos = Video.objects.filter(year_id = search_year).order_by('-year')
-        if search_year == 'all' and search_concert != 'all':
+        if search_year == 'all' and search_concert != 'all' and search_team == 'all':
             videos = Video.objects.filter(concert_id = search_concert).order_by('-year')
-        if search_year != 'all' and search_concert != 'all':
+        if search_year == 'all' and search_concert == 'all' and search_team != 'all':
+            videos = Video.objects.filter(team_id = search_team).order_by('-year')
+
+        if search_year != 'all' and search_concert != 'all' and search_team == 'all':
             videos = Video.objects.filter(year_id = search_year, concert_id = search_concert).order_by('-year')
+        if search_year == 'all' and search_concert != 'all' and search_team != 'all':
+            videos = Video.objects.filter(concert_id = search_concert, team_id = search_team).order_by('-year')
+        if search_year != 'all' and search_concert == 'all' and search_team != 'all':
+            videos = Video.objects.filter(team_id = search_team, year_id = search_year).order_by('-year')
 
     else:
-        videos = Video.objects.all()
+        videos = Video.objects.all().order_by('-year')
 
-    context = {'onenotice': onenotice, 'videos':videos, 'years':years, 'concerts':concerts}
+    context = {'onenotice': onenotice, 'videos':videos, 'teams':teams, 'years':years, 'concerts':concerts}
     return render(request, 'videos/list.html', context)
 
 def watch(request, video_pk):
@@ -36,6 +48,8 @@ def watch(request, video_pk):
 def detail(request, team_pk):
     onenotice  = get_object_or_404(Onenotice, pk=1)
     team = get_object_or_404(Team, pk=team_pk)
+    members = team.team_members.all().order_by('-part_id')
+    videos = team.video_set.all().order_by('-year')
 
-    context = {'onenotice': onenotice, 'team':team}
+    context = {'onenotice': onenotice, 'team':team, 'videos':videos, 'members':members}
     return render(request, 'videos/detail.html', context)
