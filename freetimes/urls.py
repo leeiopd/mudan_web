b08e1f6b4053e27c9f78ba148c49a17077e1c56f
leeiopd <leeiopd@hanmail.net>
2019-06-04 17:56:01 +0900

    Freetimes|완성

--- freetimes/urls.py
+++ freetimes/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'freetimes'
 
 urlpatterns = [
+    path('', views.list, name='list'),
 ]
--- freetimes/views.py
+++ freetimes/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Table, Freeweek, Freehour
+from notices.models import Onenotice
 
 # Create your views here.
+def list(request):
+    onenotice = get_object_or_404(Onenotice, pk=1)
+    freeweeks = Freeweek.objects.all()
+    freehours  = Freehour.objects.all()
+    tables = Table.objects.all()
+    context = {'tables':tables, 'freeweeks':freeweeks, 'freehours':freehours, 'onenotice':onenotice}
+    return render(request, 'freetimes/list.html', context)
