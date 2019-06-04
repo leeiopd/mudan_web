200a50e41bbbe1b210739bb659fb70906e86c751
leeiopd <leeiopd@hanmail.net>
2019-06-04 15:07:26 +0900

    Videos|list 페이지, 검색기능 완성

--- videos/urls.py
+++ videos/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'videos'
 
 urlpatterns = [
-    
+    path('', views.list, name='list'),
+    path('<int:video_pk>/watch/', views.watch, name='watch'),
+    path('<int:team_pk>/detail/', views.detail, name='detail')
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Video, Year, Concert, Team
+from notices.models import Onenotice
 # Create your views here.
+
+def list(request):
+    onenotice  = get_object_or_404(Onenotice, pk=1)
+    years = Year.objects.all()
+    concerts = Concert.objects.all()
+
+    if request.method == 'POST':
+        search_year = request.POST.get('search_year')
+        search_concert = request.POST.get('search_concert')
+        if search_year == 'all' and search_concert == 'all':
+            videos = Video.objects.all().order_by('-year')
+        if search_year != 'all' and search_concert == 'all':
+            videos = Video.objects.filter(year_id = search_year).order_by('-year')
+        if search_year == 'all' and search_concert != 'all':
+            videos = Video.objects.filter(concert_id = search_concert).order_by('-year')
+        if search_year != 'all' and search_concert != 'all':
+            videos = Video.objects.filter(year_id = search_year, concert_id = search_concert).order_by('-year')
+
+    else:
+        videos = Video.objects.all()
+
+    context = {'onenotice': onenotice, 'videos':videos, 'years':years, 'concerts':concerts}
+    return render(request, 'videos/list.html', context)
+
+def watch(request, videos_pk):
+    onenotice  = get_object_or_404(Onenotice, pk=1)
+    video = get_object_or_404(Video, pk=videos_pk)
+
+    context = {'onenotice': onenotice, 'video':video}
+    return render(request, 'videos/watch.html', context)
+
+def detail(request, team_pk):
+    onenotice  = get_object_or_404(Onenotice, pk=1)
+    team = get_object_or_404(Team, pk=team_pk)
+
+    context = {'onenotice': onenotice, 'team':team}
+    return render(request, 'videos/detail.html', context)
