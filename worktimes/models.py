0e884193d2e8f2f7b4097f30180658d05eab6c96
leeiopd <leeiopd@hanmail.net>
2019-06-05 16:26:39 +0900

    heroku배포 2차

--- worktimes/models.py
+++ worktimes/models.py
@@ -17,7 +17,7 @@ class Workhour(models.Model):
         return f'{self.clock}'
 
 class Color(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return f'{self.name}'
