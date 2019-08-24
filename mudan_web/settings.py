9010bb70a2362fb8b07f701367ecbb27a429b254
이주호 <leeiopd@hanmail.net>
2019-08-24 21:19:21 +0900

    backup

--- mudan_web/settings.py
+++ mudan_web/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'noh^dadcvssvsc8$b6x9f)75yn4^mf-@&ovd+7=17gw)%nrdc4'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -129,7 +130,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Activate Django-Heroku. 
+# Activate Django-Heroku.
 # 맨 마지막 줄
 django_heroku.settings(locals())
 
