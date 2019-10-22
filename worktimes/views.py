be336faa92383a23f6812fa0243fa6694c1b3755
이주호 <leeiopd@hanmail.net>
2019-10-22 15:47:30 +0900

    worktimes 최적화

--- worktimes/views.py
+++ worktimes/views.py
@@ -8,7 +8,13 @@ from .models import Workweek, Workhour, Table, Nowteam
 def list(request):
     onenotice = get_object_or_404(Onenotice, pk=1)
     workhours = Workhour.objects.all()
-    tables = Table.objects.all()
-    context = {'tables': tables,
+    tables1 = Table.objects.filter(week_id=1)
+    tables2 = Table.objects.filter(week_id=2)
+    tables3 = Table.objects.filter(week_id=3)
+    tables4 = Table.objects.filter(week_id=4)
+    tables5 = Table.objects.filter(week_id=5)
+    tables6 = Table.objects.filter(week_id=6)
+    tables7 = Table.objects.filter(week_id=7)
+    context = {'tables1': tables1, 'tables2': tables2, 'tables3': tables3, 'tables4': tables4, 'tables5': tables5, 'tables6': tables6, 'tables7': tables7,
                'workhours': workhours, 'onenotice': onenotice}
     return render(request, 'worktimes/list.html', context)
