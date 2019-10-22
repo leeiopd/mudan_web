321c6af0e9f61ff397459a32ab688093e2d9aabc
이주호 <leeiopd@hanmail.net>
2019-10-22 13:42:28 +0900

    worktime tamplate 재정렬

--- members/views.py
+++ members/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Member, Part, MudanNo, Alive
 from notices.models import Onenotice
 # Create your views here.
+
+
 def list(request):
     onenotice = get_object_or_404(Onenotice, pk=1)
     mudannos = MudanNo.objects.all()
@@ -14,27 +16,38 @@ def list(request):
         if search_mudanno == 'all' and search_part == 'all' and search_alive == 'all':
             members = Member.objects.all().order_by('-mudanNo')
         if search_mudanno != 'all' and search_part != 'all' and search_alive != 'all':
-            members = Member.objects.filter(mudanNo_id = search_mudanno, alive_id = search_alive, part = search_part).order_by('-mudanNo')
+            members = Member.objects.filter(
+                mudanNo_id=search_mudanno, alive_id=search_alive, part=search_part).order_by('-mudanNo')
         if search_mudanno == 'all' and search_part == 'all' and search_alive != 'all':
-            members =  Member.objects.filter(alive_id = search_alive).order_by('-mudanNo')
+            members = Member.objects.filter(
+                alive_id=search_alive).order_by('-mudanNo')
         if search_mudanno == 'all' and search_part != 'all' and search_alive == 'all':
-            members = Member.objects.filter(part_id = search_part).order_by('-mudanNo')
+            members = Member.objects.filter(
+                part_id=search_part).order_by('-mudanNo')
         if search_mudanno != 'all' and search_part == 'all' and search_alive == 'all':
-            members = Member.objects.filter(mudanNo_id = search_mudanno).order_by('-mudanNo')
+            members = Member.objects.filter(
+                mudanNo_id=search_mudanno).order_by('-mudanNo')
         if search_mudanno == 'all' and search_part != 'all' and search_alive != 'all':
-            members =  Member.objects.filter(alive_id = search_alive, part_id = search_part).order_by('-mudanNo')
+            members = Member.objects.filter(
+                alive_id=search_alive, part_id=search_part).order_by('-mudanNo')
         if search_mudanno != 'all' and search_part == 'all' and search_alive != 'all':
-            members =  Member.objects.filter(alive_id = search_alive, mudanNo_id = search_mudanno).order_by('-mudanNo')
+            members = Member.objects.filter(
+                alive_id=search_alive, mudanNo_id=search_mudanno).order_by('-mudanNo')
         if search_mudanno != 'all' and search_part != 'all' and search_alive == 'all':
-            members =  Member.objects.filter(mudanNo_id = search_mudanno, part_id = search_part).order_by('-mudanNo')
+            members = Member.objects.filter(
+                mudanNo_id=search_mudanno, part_id=search_part).order_by('-mudanNo')
     else:
-        members = Member.objects.all().order_by('-mudanNo')
-    context = {'members':members, 'onenotice':onenotice, 'mudannos':mudannos, 'parts':parts, 'alives':alives}
+        last_mudanNo = MudanNo.objects.all().last()
+        members = Member.objects.filter(
+            mudanNo_id=last_mudanNo).order_by('-mudanNo')
+    context = {'members': members, 'onenotice': onenotice,
+               'mudannos': mudannos, 'parts': parts, 'alives': alives}
     return render(request, 'members/list.html', context)
+
 
 def detail(request, member_pk):
     onenotice = get_object_or_404(Onenotice, pk=1)
     member = get_object_or_404(Member, pk=member_pk)
-    context = {'member':member, 'onenotice':onenotice}
+    context = {'member': member, 'onenotice': onenotice}
 
     return render(request, 'members/detail.html', context)
--- worktimes/views.py
+++ worktimes/views.py
@@ -3,10 +3,12 @@ from notices.models import Onenotice
 from .models import Workweek, Workhour, Table, Nowteam
 
 # Create your views here.
+
+
 def list(request):
     onenotice = get_object_or_404(Onenotice, pk=1)
-    workweeks = Workweek.objects.all()
-    workhours  = Workhour.objects.all()
+    workhours = Workhour.objects.all()
     tables = Table.objects.all()
-    context = {'tables':tables, 'workweeks':workweeks, 'workhours':workhours, 'onenotice':onenotice}
+    context = {'tables': tables,
+               'workhours': workhours, 'onenotice': onenotice}
     return render(request, 'worktimes/list.html', context)
